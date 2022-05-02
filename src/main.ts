28e5fb6d2c772aea7af59effc7036e6be8fd6c4f
Rodion-Lozovoi <rodionlozovoi@gmail.com>
2022-05-02 13:48:46 +0200

    changed port, added cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true }),
   );
-  await app.listen(3000);
+  await app.listen(3017);
 }
 bootstrap();
